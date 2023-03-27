1477d7c303c37d3a5821f5d91931558a4a7bd068
duskite <duskite@github.com>
2023-03-27 16:59:58 +0900

    임시저장

---
+++ back/src/main/java/com/dus/back/boilerplate/BoilerplateServiceImpl.java
@@ -0,0 +1,10 @@
+package com.dus.back.boilerplate;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoilerplateServiceImpl {
+
+    public BoilerplateServiceImpl() {
+    }
+}
